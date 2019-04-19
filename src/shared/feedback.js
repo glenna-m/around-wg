f3246ad63538690948c31ccacc4ace1f9f70d1e4
glenna-m <glennamayo@gmail.com>
2019-04-19 01:34:24 -0700

    feedback updates

--- src/shared/feedback.js
+++ src/shared/feedback.js
@@ -1,32 +1,32 @@
 export const FEEDBACK = [
   {
     date: "April 2, 2019",
-    name: "Glenna Mayo",
+    name: "Glenna M",
     okToContact: true,
-    phone: "408-316-0668",
-    email: "glennamayo@gmail.com",
+    phone: "408-123-4567",
+    email: "glenna@gmail.com",
     message: "Cool stuff!"
   },
   {
     date: "April 3, 2019",
-    name: "Dicie Gorker",
+    name: "Dicie G",
     okToContact: false,
-    phone: "408-265-8040",
-    email: "djgorker@gmail.com",
+    phone: "408-234-5678",
+    email: "dicie@gmail.com",
     message:
       "Confusing! I can't find anything in here - need more pictures and a lot more description."
   },
   {
     date: "April 4, 2019",
-    name: "Sheryl Sage",
+    name: "Sheryl S",
     okToContact: true,
-    phone: "408-464-0667",
-    email: "sheryl.sage@redis.com",
+    phone: "408-345-6789",
+    email: "sheryl@redis.com",
     message: "Have you thought about using Redis?  Call me!"
   },
   {
     date: "April 5, 2019",
-    name: "Charis Brandenburg",
+    name: "Charis B",
     okToContact: true,
     message: "You should add some photos of cute puppies in the garden."
   }
